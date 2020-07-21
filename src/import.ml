e5f9b66a18274e7748d45c7837b901d389ed7195
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-07-20 19:12:16 -0700

    ocamlformat and some structure stuff

--- src/import.ml
+++ src/import.ml
@@ -1,13 +1,18 @@
+external add_function_import :
+  Module.t -> string -> string -> string -> Type.t -> Type.t -> unit
+  = "caml_binaryen_add_function_import__bytecode" "caml_binaryen_add_function_import"
 (** Module, internal name, external module name, external base name, params type, results type. *)
-external add_function_import : Module.t -> string -> string -> string -> Type.t -> Type.t -> unit =
-  "caml_binaryen_add_function_import__bytecode" "caml_binaryen_add_function_import"
 
+external add_table_import : Module.t -> string -> string -> string -> unit
+  = "caml_binaryen_add_table_import"
 (** Module, internal name, external module name, external base name. *)
-external add_table_import : Module.t -> string -> string -> string -> unit = "caml_binaryen_add_table_import"
 
+external add_memory_import :
+  Module.t -> string -> string -> string -> bool -> unit
+  = "caml_binaryen_add_memory_import"
 (** Module, internal name, external module name, external base name, shared. *)
-external add_memory_import : Module.t -> string -> string -> string -> bool -> unit = "caml_binaryen_add_memory_import"
 
+external add_global_import :
+  Module.t -> string -> string -> string -> Type.t -> bool -> unit
+  = "caml_binaryen_add_global_import__bytecode" "caml_binaryen_add_global_import"
 (** Module, internal name, external module name, external base name, type, mutable. *)
-external add_global_import : Module.t -> string -> string -> string -> Type.t -> bool -> unit = 
-  "caml_binaryen_add_global_import__bytecode" "caml_binaryen_add_global_import"
