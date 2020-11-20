25400331febe87f1907c6b73a518c98fd0d6bd2e
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-19 17:32:34 -0700

    combine path/no_dynlink & artifact stuff

--- config/discover.ml
+++ config/discover.ml
@@ -2,18 +2,15 @@ module C = Configurator.V1
 
 let () =
   C.main ~name:"binaryen" (fun c ->
-      let default : C.Pkg_config.package_conf =
-        {
-          libs = [ "-lstdc++"; "-lpthread" ];
-          cflags = [ "-O2"; "-Wall"; "-Wextra" ];
-        }
-      in
+      let default : C.Pkg_config.package_conf = { libs = []; cflags = [] } in
       let conf =
         match C.ocaml_config_var c "system" with
         | None -> default
-        | Some ("linux" | "macosx") ->
-            { default with libs = [ "-lstdc++"; "-lpthread" ] }
-        | Some "mingw64" -> default
+        | Some ("linux" | "macosx" | "mingw64") ->
+            {
+              libs = [ "-lstdc++"; "-lpthread" ];
+              cflags = [ "-O2"; "-Wall"; "-Wextra" ];
+            }
         | Some unknown ->
             failwith ("We don't know how to build for platform: " ^ unknown)
       in
