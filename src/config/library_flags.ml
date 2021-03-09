0b920431372119d92b0077f51b91ca745fbfde87
Oscar Spencer <oscar@grain-lang.org>
2021-03-08 22:40:44 -0500

    fix: Module.optimize on macOS (#77)

---
+++ src/config/library_flags.ml
@@ -0,0 +1,17 @@
+module C = Configurator.V1
+
+let () =
+  C.main ~name:"library_flags" (fun c -> 
+    let default = [] in
+
+    let library_flags =
+      match C.ocaml_config_var c "system" with
+      | Some("macosx") ->
+        (* These flags preserve the original C error behavior *)
+        ["-cclib"; "-Wl,-keep_dwarf_unwind"]
+      | Some(_) -> default
+      | None -> default
+    in
+
+    C.Flags.write_sexp "library_flags.sexp" library_flags
+  )
