14e178c20060c1f5e39fbe30be1b15a1cbe517ce
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-03-15 12:34:39 -0700

    null value?

--- js/expression.ml
+++ js/expression.ml
@@ -179,9 +179,7 @@ let tuple_extract wasm_mod tuple index =
 let pop wasm_mod typ =
   meth_call global##.binaryen "_BinaryenPop" [| inject wasm_mod; inject typ |]
 
-let null wasm_mod =
-  let scope = get wasm_mod "ref" in
-  meth_call scope "null" [||]
+let null wasm_mod = null
 
 let print expr =
   let text = meth_call global##.binaryen "emitText" [| inject expr |] in
