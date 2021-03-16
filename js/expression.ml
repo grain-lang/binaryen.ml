61861e2590816bccc5c6c17a2a5b77f3a3db3779
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-03-16 11:06:33 -0700

    fix: return JS null value from Expression.null (#83)

--- js/expression.ml
+++ js/expression.ml
@@ -179,8 +179,7 @@ let tuple_extract wasm_mod tuple index =
 let pop wasm_mod typ =
   meth_call global##.binaryen "_BinaryenPop" [| inject wasm_mod; inject typ |]
 
-(* TODO: Figure this out *)
-let null () = Obj.magic ()
+let null () = pure_js_expr "null"
 
 let print expr =
   let text = meth_call global##.binaryen "emitText" [| inject expr |] in
