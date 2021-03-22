daaa80045dc098dd4b065986cdeed60a8bf2d764
Oscar Spencer <oscar@grain-lang.org>
2021-03-21 22:29:41 -0400

    feat: Add operations on globals

--- js/global.ml
+++ js/global.ml
@@ -17,3 +17,25 @@ let get_global wasm_mod name =
 
 let remove_global wasm_mod name =
   meth_call wasm_mod "removeGlobal" [| inject (string name) |]
+
+let get_num_globals wasm_mod =
+  meth_call global##.binaryen "_BinaryenGetNumGlobals" [| inject wasm_mod |]
+
+let get_global_by_index wasm_mod index =
+  meth_call global##.binaryen "_BinaryenGlobalByIndex"
+    [| inject wasm_mod; inject index |]
+
+let get_name global_ =
+  to_string
+    (meth_call global##.binaryen "_BinaryenGlobalGetName" [| inject global_ |])
+
+let get_type global_ =
+  meth_call global##.binaryen "_BinaryenGlobalGetType" [| inject global_ |]
+
+let is_mutable global_ =
+  to_bool
+    (meth_call global##.binaryen "_BinaryenGlobalIsMutable"
+       [| inject global_ |])
+
+let get_init_expr global_ =
+  meth_call global##.binaryen "_BinaryenGlobalGetInitExpr" [| inject global_ |]
