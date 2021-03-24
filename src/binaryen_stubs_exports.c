6410fd5efae09282bd70d3e52e6beaa92df55d54
Oscar Spencer <oscar@grain-lang.org>
2021-03-23 22:55:59 -0400

    feat: Add operations on exports

--- src/binaryen_stubs_exports.c
+++ src/binaryen_stubs_exports.c
@@ -2,6 +2,7 @@
 #include <caml/mlvalues.h>
 #include <caml/fail.h>
 #include <caml/memory.h>
+#include <caml/alloc.h>
 
 #include "binaryen-c.h"
 #include "ocaml_helpers.h"
@@ -56,10 +57,50 @@ caml_binaryen_add_global_export(value _module, value _internalName, value _exter
 }
 
 CAMLprim value
+caml_binaryen_get_export(value _module, value _externalName) {
+  CAMLparam2(_module, _externalName);
+  BinaryenModuleRef module = BinaryenModuleRef_val(_module);
+  char* externalName = Safe_String_val(_externalName);
+  BinaryenExportRef exp = BinaryenGetExport(module, externalName);
+  CAMLreturn(alloc_BinaryenExportRef(exp));
+}
+
+CAMLprim value
 caml_binaryen_remove_export(value _module, value _externalName) {
   CAMLparam2(_module, _externalName);
   BinaryenModuleRef module = BinaryenModuleRef_val(_module);
   char* externalName = Safe_String_val(_externalName);
   BinaryenRemoveExport(module, externalName);
   CAMLreturn(Val_unit);
+}
+
+CAMLprim value
+caml_binaryen_get_num_exports(value _module) {
+  CAMLparam1(_module);
+  BinaryenModuleRef module = BinaryenModuleRef_val(_module);
+  BinaryenIndex num = BinaryenGetNumExports(module);
+  CAMLreturn(Val_int(num));
+}
+
+CAMLprim value
+caml_binaryen_get_export_by_index(value _module, value _index) {
+  CAMLparam2(_module, _index);
+  BinaryenModuleRef module = BinaryenModuleRef_val(_module);
+  BinaryenIndex index = Int_val(_index);
+  BinaryenExportRef exp = BinaryenGetExportByIndex(module, index);
+  CAMLreturn(alloc_BinaryenExportRef(exp));
+}
+
+CAMLprim value
+caml_binaryen_export_get_name(value _exp) {
+  CAMLparam1(_exp);
+  BinaryenExportRef exp = BinaryenExportRef_val(_exp);
+  CAMLreturn(caml_copy_string(BinaryenExportGetName(exp)));
+}
+
+CAMLprim value
+caml_binaryen_export_get_value(value _exp) {
+  CAMLparam1(_exp);
+  BinaryenExportRef exp = BinaryenExportRef_val(_exp);
+  CAMLreturn(caml_copy_string(BinaryenExportGetValue(exp)));
 }
--- src/export.ml
+++ src/export.ml
@@ -16,6 +16,21 @@ external add_global_export : Module.t -> string -> string -> t
   = "caml_binaryen_add_global_export"
 (** Module, internal name, external name. *)
 
+external get_export : Module.t -> string -> t = "caml_binaryen_get_export"
+
 external remove_export : Module.t -> string -> unit
   = "caml_binaryen_remove_export"
 (** Module, external name. *)
+
+external get_num_exports : Module.t -> int = "caml_binaryen_get_num_exports"
+
+external get_export_by_index : Module.t -> int -> t
+  = "caml_binaryen_get_export_by_index"
+
+external get_name : t -> string = "caml_binaryen_export_get_name"
+
+external get_value : t -> string = "caml_binaryen_export_get_value"
+
+external get_name : t -> string = "caml_binaryen_export_get_name"
+
+external get_value : t -> string = "caml_binaryen_export_get_value"
