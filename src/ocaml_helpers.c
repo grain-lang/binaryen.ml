bae3f7a491371f70ba3a0e6f7a3927dcbb5e9c7d
Philip Blair <philip@pblair.org>
2021-06-20 12:30:40 +0200

    Move stubs back to ocaml_helpers to see if that fixes the issue

--- src/ocaml_helpers.c
+++ src/ocaml_helpers.c
@@ -37,3 +37,15 @@ int array_length(value array) {
   res = caml_callback(*closure_f, array);
   CAMLreturnT(int, Int_val(res));
 }
+
+int __sprintf_chk(char * str, int flag, size_t strlen, const char * format) {
+  sprintf(str, flag, strlen, format);
+}
+
+typedef void (*dtor_func) (void *);
+int __cxa_thread_atexit_impl(dtor_func func, void *obj, void *dso_symbol) {
+  // This is the destructor for C++ thread-local variables. It appears that there is only one such
+  // variable in Binaryen: https://github.com/WebAssembly/binaryen/blob/b2c63a9665a9758c50eac60af605f0399f66580f/src/emscripten-optimizer/istring.h#L74
+  // For now, we don't worry about it. We may need to reimplement this function in the future, though.
+  return 0;
+}
