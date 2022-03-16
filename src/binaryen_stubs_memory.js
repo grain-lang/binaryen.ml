5fb26ceac9b8542ef562c55fbe641e6ba5cc4519
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-03-15 18:25:52 -0700

    fix: Make memory segments work with jsoo

--- src/binaryen_stubs_memory.js
+++ src/binaryen_stubs_memory.js
@@ -1,10 +1,10 @@
 //Provides: caml_binaryen_set_memory
-//Requires: caml_list_to_js_array, caml_jsstring_of_string, caml_js_from_bool
+//Requires: caml_list_to_js_array, caml_convert_bytes_to_array
+//Requires: caml_jsstring_of_string, caml_js_from_bool
 function caml_binaryen_set_memory(wasm_mod, initial, maximum, exportName, segmentNames, segmentPassives, segmentOffsets, segmentSizes, shared) {
   var segs = caml_list_to_js_array(segmentNames).map(function (segmentName, idx) {
-    // TODO: Check this
     return {
-      data: Uint8Array.from(caml_jsstring_of_string(segmentName)),
+      data: caml_convert_bytes_to_array(segmentName),
       passive: segmentPassives[idx + 1], // OCaml Lists are offset by 1
       offset: segmentOffsets[idx + 1] // OCaml Lists are offset by 1
     }
