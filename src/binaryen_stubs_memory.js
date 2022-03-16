0be4b410161d3bfc531a18a5930f1f4d82b74b1a
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-03-16 15:28:17 -0700

    chore: Rename in JS too

--- src/binaryen_stubs_memory.js
+++ src/binaryen_stubs_memory.js
@@ -1,10 +1,10 @@
 //Provides: caml_binaryen_set_memory
 //Requires: caml_list_to_js_array, caml_convert_bytes_to_array
 //Requires: caml_jsstring_of_string, caml_js_from_bool
-function caml_binaryen_set_memory(wasm_mod, initial, maximum, exportName, segmentNames, segmentPassives, segmentOffsets, segmentSizes, shared) {
-  var segs = caml_list_to_js_array(segmentNames).map(function (segmentName, idx) {
+function caml_binaryen_set_memory(wasm_mod, initial, maximum, exportName, segments, segmentPassives, segmentOffsets, segmentSizes, shared) {
+  var segs = caml_list_to_js_array(segments).map(function (segment, idx) {
     return {
-      data: caml_convert_bytes_to_array(segmentName),
+      data: caml_convert_bytes_to_array(segment),
       passive: segmentPassives[idx + 1], // OCaml Lists are offset by 1
       offset: segmentOffsets[idx + 1] // OCaml Lists are offset by 1
     }
