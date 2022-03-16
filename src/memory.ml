d55e5773d39fd5f21f556e0f5493d6c60bbc2198
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-03-16 16:25:09 -0700

    feat!: Rename segment name to data (#146)

--- src/memory.ml
+++ src/memory.ml
@@ -11,7 +11,7 @@ external set_memory :
   unit = "caml_binaryen_set_memory__bytecode" "caml_binaryen_set_memory"
 
 type segment = {
-  name : string;
+  data : string;
   passive : bool;
   offset : Expression.t;
   size : int;
@@ -22,18 +22,18 @@ let set_memory wasm_mod initial maximum export_name (segments : segment list)
     shared =
   let split_segments segments =
     List.fold_right
-      (fun { name; passive; offset; size }
-           (segment_names, segment_passive, segment_offsets, segment_sizes) ->
-        ( name :: segment_names,
+      (fun { data; passive; offset; size }
+           (segment_data, segment_passive, segment_offsets, segment_sizes) ->
+        ( data :: segment_data,
           passive :: segment_passive,
           offset :: segment_offsets,
           size :: segment_sizes ))
       segments ([], [], [], [])
   in
-  let segment_names, segment_passive, segment_offsets, segment_sizes =
+  let segment_data, segment_passive, segment_offsets, segment_sizes =
     split_segments segments
   in
-  set_memory wasm_mod initial maximum export_name segment_names segment_passive
+  set_memory wasm_mod initial maximum export_name segment_data segment_passive
     segment_offsets segment_sizes shared
 
 let unlimited = -1
