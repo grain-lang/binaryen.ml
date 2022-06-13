a8f3da0db3e8fc00a5f50bb1d17d2bbf3e68b6b0
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-06-13 13:46:30 -0700

    fix: Convert JSOO lists to JS arrays before working with them

--- src/binaryen_stubs_memory.js
+++ src/binaryen_stubs_memory.js
@@ -2,11 +2,13 @@
 //Requires: caml_list_to_js_array, caml_convert_bytes_to_array
 //Requires: caml_jsstring_of_string, caml_js_from_bool
 function caml_binaryen_set_memory(wasm_mod, initial, maximum, exportName, segments, segmentPassives, segmentOffsets, segmentSizes, shared) {
+  var passives = caml_list_to_js_array(segmentPassives);
+  var offsets = caml_list_to_js_array(segmentOffsets);
   var segs = caml_list_to_js_array(segments).map(function (segment, idx) {
     return {
       data: caml_convert_bytes_to_array(segment),
-      passive: caml_js_from_bool(segmentPassives[idx + 1]), // OCaml Lists are offset by 1
-      offset: segmentOffsets[idx + 1] // OCaml Lists are offset by 1
+      passive: caml_js_from_bool(passives[idx]),
+      offset: offsets[idx]
     }
   });
 
