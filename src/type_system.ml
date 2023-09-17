54dd4bf11ccd35627f7b6221e33bea7a2f705d37
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-09-16 17:54:47 -0700

    chore!: Remove `type_system`

--- src/type_system.ml
+++
@@ -1,6 +0,0 @@
-type t = int
-
-external nominal : unit -> t = "caml_binaryen_type_system_nominal"
-external isorecursive : unit -> t = "caml_binaryen_type_system_isorecursive"
-external get_type_system : unit -> t = "caml_binaryen_get_type_system"
-external set_type_system : t -> unit = "caml_binaryen_set_type_system"
